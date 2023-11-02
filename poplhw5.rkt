beb98c6df9c940fd52b72ba6c076ce6292f72ea0
Ethan Tillotson <89542030+rllycool@users.noreply.github.com>
2023-11-02 13:31:38 -0500

    Add files via upload

--- poplhw5.rkt
+++ poplhw5.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+;add 
 (define (poly_add apol bpol)
   (if (null? apol) bpol
       (if (null? bpol) apol
@@ -10,10 +11,12 @@
 (define (addup l1 l2) ; From Lecture
   (if (null? l1) l2
       (if (null? l2) l1
-          (cons (+ (car l1) (car l2))
+ 
+          ( cons (+ (car l1) (car l2))
                 (addup (cdr l1) (cdr l2)))))
   )
-
+z
+;sub
 (define (poly_sub apol bpol)
    (if (null? apol) bpol
       (if (null? bpol) apol
@@ -24,4 +27,39 @@
 (define (subup l1 l2)
   (if (null? l1) l2
       (if (null? l2) l1
-          (cons(- (car l2) (car l1)) subup(cdr l1) cdr l2))))
+          (cons(- (car l1) (car l2))
+               (subup(cdr l1) (cdr l2)))))
+ )
+
+(define (mulup l1 l2)  ; From Lecture
+  (if (null? l1) '()
+      (if (null? l2)'()
+          (addup(smup (car l1) l2)
+                (cons 0 (mulup (cdr l1) l2)))))
+)
+
+(define (smup a l1)  ; From Lecture
+  (if (null? l1) '()
+      (cons (* a (car l1))
+            (smup a (cdr l1))))
+)
+
+;multiplication
+(define (poly-list-of-list l1 L2)
+  (if (null? l1) '()
+      (if (null? L2) '()
+          (cons (mulup l1 (car L2))
+            (poly-list-of-list l1 (cdr L2)))))
+)
+
+(define (poly_mul apol bpol)
+  (if(null? apol) '()
+     (if(null? bpol) '()
+        (poly_add(poly-list-of-list (car apol) bpol)
+                (cons '() (poly_mul (cdr apol) bpol)))))
+  )
+
+        
+(define (checkempty l1 l2)
+  (cond (0 car l1) (cons('())
+               (cdr l1))))
